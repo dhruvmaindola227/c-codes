bb033f36a81c621d66ca7cc697c46e44477f6354
Dhruv <dhruv.maindola227>
2022-08-08 14:21:15 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+     class Program
+    {
+        String? firstName;
+        String? lastName;
+        String? age;
+        int salary;
+
+        void display(){
+                Console.WriteLine(this.firstName + " " + this.lastName + " " + this.age + " " + this.salary);
+        }
+
+        Program(){
+
+        }
+
+        Program(Program pro){
+            firstName = pro.firstName;
+            lastName = pro.lastName;
+            age = pro.age;
+            salary = pro.salary;
+        }
+
+        static void Main(string[] args)
+        {
+            Program pr = new Program();
+            Program pr1 = new Program(pr);
+            pr1.display();
+        }
+    }
+
---
+++ fn.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace newname // Note: actual namespace depends on the project name.
+{
+     class fn
+    {
+            public static void addTwoNums(int a , int b){
+                Console.WriteLine("a is "+ a + " and b is " + b + " and sum is " + (a + b));
+            }
+    }
+}
